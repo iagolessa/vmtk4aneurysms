a8b6d5d010fd971b757bb829aa7c39c19078fd69
iagolessa <iago.42lessa@gmail.com>
2019-01-14 17:16:52 -0200

    Updated final functions to compute statistics of WSS and OSI. Updated module file.

--- aneurysms.py
+++ aneurysms.py
@@ -1,56 +1,128 @@
-#! /usr/bin/env python
 
 import paraview.simple as pv
 import numpy as np
 
+def area_averaged_wss(case):
+    """ Function that calculates the area-averaged WSS
+        for a surface where the wall shear stress field
+        is defined. The function takes its input an 
+        OpenFOAM case reader with the surface and fields.
+        It automatically selects the wallShearComponent field
+        and the surface wall. It returns an array with time
+        in one column and the result in the other. 
+    """
+    # Update arrays to be used:
+    # - wallShearComponent
+    
+
+    case.CellArrays = ['wallShearComponent']
+
+    # And select the surface where integration will be carried out
+    case.MeshRegions = ['wall']
+
+    # Get time-steps values
+    timeSteps = np.array(case.TimestepValues)
+    # Update time-step
+    case.UpdatePipeline()
+
+    # Integrate WSS on the wall
+    computeWallArea = pv.IntegrateVariables()
+    computeWallArea.Input = case
+    computeWallArea.UpdatePipeline()
+    # Get area of surface, in m2
+    wallArea = computeWallArea.CellData.GetArray('Area').GetRange()[0]
+
+    areaAveragedWSSList = []
+    for timeStep in timeSteps:
+        # Calculate WSS magnitude
+        # Instantiate calculater filter
+        calcMagWSS = pv.Calculator()
+        calcMagWSS.Input = case
+        calcMagWSS.ResultArrayName = 'magWSS'
+        calcMagWSS.Function = '1056*mag(wallShearComponent)'
+        calcMagWSS.AttributeType = 'Cell Data'
+        calcMagWSS.UpdatePipeline(time=timeStep)
+
+        # Integrate WSS on the wall
+        integrateWSS = pv.IntegrateVariables()
+        integrateWSS.Input = calcMagWSS
+        integrateWSS.UpdatePipeline(time=timeStep)
+
+        # Instantiate calculater filter
+        areaAveragedWSS = pv.Calculator()
+        areaAveragedWSS.Input = integrateWSS
+        areaAveragedWSS.ResultArrayName = 'areaAveragedWSS'
+        areaAveragedWSS.Function = 'magWSS/'+str(wallArea)
+        areaAveragedWSS.AttributeType = 'Cell Data'
+        areaAveragedWSS.UpdatePipeline(time=timeStep)
+        areaAveragedWSSList.append([timeStep, 
+                                    areaAveragedWSS.CellData.GetArray('areaAveragedWSS').GetRange()[0]])
+
+    return np.asarray(areaAveragedWSSList)
+
+
 def osi(ofCaseFile, 
         timeIndexRange, 
-        outputFileName, 
+        outputFileName,
+        timeStep=0.01,
+        density=1056.0, # kg/m3
         wssFieldName='wallShearComponent', 
         patchName='wall'):
-    """ Function to calculate the oscillatory shear index
-        for a time interval [0,T] indentified by time step indices.
-        The method (based o VTK), ignores the time-step size
-        and consider uniform time stepping (so, if the time-step
-        is large, the resulting field may be large). The OSI 
-        field is defined as:
+    """ 
+        Function to calculate the oscillatory shear index and
+        other time integrals variables of WSS over a time inter-
+        val [Ti,Tf] indentified by time-step indices. The method
+        (based o VTK), ignores the time-step size and 
+        consider uniform time stepping (so, if the time-step is 
+        large, the resulting fields may be very different if a va-
+        riable time-step would be considered). The OSI field is 
+        defined as:
         
-            OSI = 0.5*( 1 - norm2(int WSS dt) / int norm2(WSS) dt )
+            OSI = 0.5*( 1 - norm2(int WSS dt) / int norm2(WSS) dt)
             
-        where int implies the integral over time between instants
-        t1 and t2 (usually for a cardiac cycle, therefore 
-        [t1, t2] = [0, T]) and norm2 is the L2 norm an Euclidean
-        vector field; WSS is the wall shear stress defined on the
-        input surface.
-        
+        where "int" implies the integral over time between two
+        instants t1 and t2 (usually for a cardiac cycle, therefore 
+        [t1, t2] = [Ti, Tf]) and norm2 is the L2 norm of an Eucli-
+        dean vector field; WSS is the wall shear stress defined on 
+        the input surface. Since this function use OpenFOAM data, 
+        please specify the density considered.
         
         Input args:
         - OpenFOAM case file (str): name of OpenFOAM .foam case;
-        - wssFieldName (str): string containing the name of the
-            wall shear stress field (default="wallShearComponent");
-        - patchName (str): patch name where to calculate OSI
-            (default="wall");
+        - wssFieldName (str, optional): string containing the name 
+            of the wall shear stress field (default="wallShearComp-
+            onent");
+        - patchName (str, optional): patch name where to calculate 
+            the OSI (default="wall");
         - timeIndexRange (list): list of initial and final time-
-            steps indices limits of the integral [0, T];
+            steps indices limits of the integral [Ti, Tf];
         - outputFileName (str): file name for the output file with 
             osi field (must be a .vtp file).
+        - blood density (float, optional): default 1056.0 kg/m3
     """
     case = pv.OpenFOAMReader(FileName=ofCaseFile)
-    
+
     # First we define only the field that are going to be used: the WSS on the aneurysm wall
     case.CellArrays = [wssFieldName]
     case.MeshRegions = [patchName]
     case.Createcelltopointfiltereddata = 0
-    
+
+    # Multiplying WSS per density
+    densityTimesWSS = pv.Calculator()
+    densityTimesWSS.Input = case
+    densityTimesWSS.AttributeType = 'Cell Data'
+    densityTimesWSS.ResultArrayName = "WSS"
+    densityTimesWSS.Function = str(density)+"*"+wssFieldName
+    densityTimesWSS.UpdatePipeline()
+
     # Calculating the magnitude of the wss vector
     calcMagWSS = pv.Calculator()
-
-    calcMagWSS.Input = case
+    calcMagWSS.Input = densityTimesWSS
     calcMagWSS.AttributeType = 'Cell Data'
-    calcMagWSS.ResultArrayName = wssFieldName+"_magnitude"
-    
+    calcMagWSS.ResultArrayName = densityTimesWSS.ResultArrayName+"_magnitude"
+
     ## Get WSS field name
-    wss = case.CellData.GetArray(wssFieldName).GetName()
+    wss = densityTimesWSS.ResultArrayName
     calcMagWSS.Function = "mag("+wss+")"
     calcMagWSS.UpdatePipeline()
 
@@ -60,8 +132,12 @@ def osi(ofCaseFile,
     timeInterval.SelectionMode = "Select Time Range"
     timeInterval.TimeStepRange = timeIndexRange #[99, 199] # range in index
     timeInterval.UpdatePipeline()
-    
+
+    # Period given by time-steps
+    period = (timeInterval.TimeStepRange[1] - timeInterval.TimeStepRange[0])*timeStep
+
     # Now compute the temporal statistics
+    # filter computes the average values of all fields
     calcAvgWSS = pv.TemporalStatistics()
 
     calcAvgWSS.Input = timeInterval
@@ -71,6 +147,7 @@ def osi(ofCaseFile,
     calcAvgWSS.ComputeStandardDeviation = 0
     calcAvgWSS.UpdatePipeline()
 
+    # Calculates OSI
     calcOSI = pv.Calculator()
     calcOSI.Input = calcAvgWSS
     calcOSI.ResultArrayName = 'OSI'
@@ -78,106 +155,63 @@ def osi(ofCaseFile,
 
     # Getting fields:
     # - Get the average of the vector WSS field
-    avgVecWSS = calcAvgWSS.CellData.GetArray(wssFieldName+"_average").GetName()
+    avgVecWSS = calcAvgWSS.CellData.GetArray(wss+"_average").GetName()
     # - Get the average of the magnitude of the WSS field 
     avgMagWSS = calcAvgWSS.CellData.GetArray(calcMagWSS.ResultArrayName+"_average").GetName()
 
-    # Compute OSI
     calcOSI.Function = "0.5*( 1 - ( mag( "+avgVecWSS+" ) )/"+avgMagWSS+" )"
     calcOSI.UpdatePipeline()
 
+    # Compute Relative Residance Time
+    calcRRT = pv.Calculator()
+    calcRRT.Input = calcOSI
+    calcRRT.ResultArrayName = 'RRT'
+    calcRRT.AttributeType = 'Cell Data'
+    calcRRT.Function = str(period)+"/mag("+avgVecWSS+")"
+    calcRRT.UpdatePipeline()
+
+    # Final processing of surface: merge blocks
+    # and get surface for triangulation
     mergeBlocks = pv.MergeBlocks()
-    mergeBlocks.Input = calcOSI
+    mergeBlocks.Input = calcRRT
     mergeBlocks.UpdatePipeline()
 
     extractSurface = pv.ExtractSurface()
     extractSurface.Input = mergeBlocks
     extractSurface.UpdatePipeline()
-    
+
     triangulate = pv.Triangulate()
     triangulate.Input = extractSurface
     triangulate.UpdatePipeline()
-    
+
     pv.SaveData(outputFileName,triangulate)
+
     
-    
-def area_averaged_wss(case):
-    """ Function that calculates the area-averaged WSS
-        for a surface where the wall shear stress field
-        is defined. The function takes its input an 
-        OpenFOAM case reader with the surface and fields.
-        It automatically selects the wallShearComponent field
-        and the surface wall.
+def wss_statistics(timeAveragedSurface, aneurysmNeckArrayName, neckIsoValue=0.5):
     """
-    # Update arrays to be used:
-    # - wallShearComponent
-    case.CellArrays = ['wallShearComponent']
-    
-    # And select the surface where integration will be carried out
-    case.MeshRegions = ['wall']
-    
-    # Get time-steps values
-    timeSteps = np.array(case.TimestepValues)
-    # Update time-step
-    case.UpdatePipeline()
+        Computes surface-averaged and maximum value 
+        of time-averaged WSS for an aneurysm surface.
+        Input is a PolyData surface with the averaged
+        fields and the aneurysm neck contour field. 
+        Return list with aneurysm area, WSSav and 
+        WSSmax.
+    """
+    clipAneurysmNeck = pv.Clip()
+    clipAneurysmNeck.Input = timeAveragedSurface
+    clipAneurysmNeck.ClipType = 'Scalar'
+    clipAneurysmNeck.Scalars  = [timeAveragedSurface.PointData.GetArray(aneurysmNeckArrayName).Name]
+    clipAneurysmNeck.Invert   = 1   # gets smaller portion
+    clipAneurysmNeck.Value    = neckIsoValue  # based on the definition of field ContourScalars
+    clipAneurysmNeck.UpdatePipeline()
 
-    # Integrate WSS on the wall
-    integrateWSS = pv.IntegrateVariables()
-    integrateWSS.Input = case
-    integrateWSS.UpdatePipeline()
+    # Finaly we integrate over Sa 
+    integrateOverAneurysm = pv.IntegrateVariables()
+    integrateOverAneurysm.Input = clipAneurysmNeck
+    integrateOverAneurysm.UpdatePipeline()
 
-    # Get area of surface, in m2
-    wallArea = integrateWSS.CellData.GetArray('Area').GetRange()[0]
+    aneurysmArea = integrateOverAneurysm.CellData.GetArray("Area").GetRange()[0] # aneurysm area
 
-    # Instantiate calculater filter
-    calcWSS = pv.Calculator()
+    WSSav = integrateOverAneurysm.CellData.GetArray("WSS_magnitude_average").GetRange()[0]/aneurysmArea # averaged
+    WSSmax = clipAneurysmNeck.CellData.GetArray("WSS_magnitude_average").GetRange()[1] # maximum value
 
-    areaAveragedWSS = []
-    for timeStep in timeSteps:  
-        # Calculate WSS magnitude
-        calcWSS.Input = integrateWSS
-        calcWSS.ResultArrayName = 'areaAveragedWSS'
-        calcWSS.Function = '(1/'+str(wallArea)+')*1056*mag('+integrateWSS.CellData.GetArray('wallShearComponent').Name+')'
-        calcWSS.AttributeType = 'Cell Data'
-        calcWSS.UpdatePipeline(time=timeStep)
-        areaAveragedWSS.append([timeStep, 
-                                calcWSS.CellData.GetArray('areaAveragedWSS').GetRange()[0]])
-    
-    return np.asarray(areaAveragedWSS)
-
-
-# # Inform the path to the .foam files
-# # THis calculation considers three meshes used for the study
-# # meshCoarse = pv.OpenFOAMReader(FileName='/home/iagolessa/foam/iagolessa-4.0/run/aneurysms/ruptured/fluidFlow/Newtonian/case1/mesh700k/mesh700k.foam')
-# # meshIntermediate = pv.OpenFOAMReader(FileName='/home/iagolessa/foam/iagolessa-4.0/run/aneurysms/ruptured/fluidFlow/Newtonian/case1/mesh1500k/mesh1500k.foam')
-# # meshFine = pv.OpenFOAMReader(FileName='/home/iagolessa/foam/iagolessa-4.0/run/aneurysms/ruptured/fluidFlow/Newtonian/case1/mesh3000k/mesh3000k.foam')
-
-# pathToFoamCase = "/home/iagolessa/foam/iagolessa-4.0/run/aneurysms/unruptured/fluidFlow/Newtonian/case17/mesh1100k/" 
-# foamFileName = "case17Newtonian.foam"
-# ofData = pv.OpenFOAMReader(FileName=pathToFoamCase+foamFileName)
-
-# # meshCoarse.SkipZeroTime = 0
-# # meshCoarse.Adddimensionalunitstoarraynames = 1
-# # ofData.MeshRegions.Available, ofDataFile.CellArrays
-
-
-# meshIntermediateWSS = area_averaged_wss(meshIntermediate)
-# meshFineWSS = area_averaged_wss(meshFine)
-
-# import matplotlib.pyplot as plt
-# import seaborn as sb
-
-# plt.style.use('classic')
-
-# %matplotlib widget
-
-# fig = plt.figure()
-
-# plt.plot(meshIntermediateWSS[:,0], meshIntermediateWSS[:,1], 'b')
-# plt.plot(meshFineWSS[:,0], meshFineWSS[:,1], 'r')
-
-# plt.xlabel('Time')
-# plt.ylabel('Area-averaged WSS (Pa)')
-
-# plt.grid()
-# plt.show()
+    return [aneurysmArea, WSSav, WSSmax]
