541806b353fa07595dc9fd88bc8486992c83f0be
Iago Lessa de Oliveira <iago.lessa.oliveira@gmail.com>
2020-07-15 16:36:08 +0100

    [add]: function to compute the mean and Gaussian curvatures of a surface; also identifies the local shape type.

--- polydatageometry.py
+++ polydatageometry.py
@@ -11,11 +11,12 @@ from constants import *
 
 def distance(point1, point2):
     sqrDistance = vtk.vtkMath.Distance2BetweenPoints(
-                    point1,
-                    point2
-                )
-    
+        point1,
+        point2
+    )
+
     return math.sqrt(sqrDistance)
+
 
 def surfaceArea(surface):
     """Compute the surface area of an input surface."""
@@ -25,6 +26,7 @@ def surfaceArea(surface):
     surface_area.Update()
 
     return surface_area.GetSurfaceArea()
+
 
 def surfaceVolume(surface):
     """Compute voluem of closed surface.
@@ -39,6 +41,7 @@ def surfaceVolume(surface):
     volume.Update()
 
     return volume.GetVolume()
+
 
 def contourPerimeter(contour):
     """Compute the perimeter of a contour defined in 3D space."""
@@ -59,6 +62,7 @@ def contourPerimeter(contour):
 
     return perimeter
 
+
 def contourBarycenter(contour):
     """Return contour barycenter."""
 
@@ -72,6 +76,7 @@ def contourBarycenter(contour):
     )
 
     return tuple(barycenter)
+
 
 def contourPlaneArea(contour):
     """Compute plane surface area enclosed by a 3D contour path."""
@@ -95,16 +100,18 @@ def contourPlaneArea(contour):
     return computeArea.GetSurfaceArea()
 
     # if computeArea.GetSurfaceArea() == 0.0:
-        # raise ValueError
+    # raise ValueError
     # else:
-        # return computeArea.GetSurfaceArea()
+    # return computeArea.GetSurfaceArea()
 #
 # except ValueError:
     # # Alternative procedure based on closing the aneurysm
     # pass
 
-## TODO: overload this functions: receive a surface closed too
-## investigate functionoverloading for modules in Python
+# TODO: overload this functions: receive a surface closed too
+# investigate functionoverloading for modules in Python
+
+
 def contourHydraulicDiameter(contour):
     """Compute hydraulic diameter of a plane contour."""
 
@@ -114,6 +121,7 @@ def contourHydraulicDiameter(contour):
     # Return hydraulic diameter of neck
     return intFour*area/perimeter
 
+
 def contourIsClosed(contour):
     """Check if contour (vtkPolyData) is closed."""
     nVertices = contour.GetNumberOfPoints()
@@ -122,4 +130,116 @@ def contourIsClosed(contour):
     return nVertices == nEdges
 
 
+def surfaceCurvature(surface):
+    """Compute curvature of surface.
 
+    Uses VTK to compute the mean and Gauss curvature 
+    of a surface represented as a vtkPolydata. Also
+    computes a integer array that identify the local
+    shape of a surface, as presented by Ma et al. (2004)
+    for intracranial aneurysms, if Kg and Km are the 
+    Gauss and mean curvature, we have:
+
+        Kg   Km     Local Shape         Int Label
+        > 0  > 0    Elliptical Convex   0
+        > 0  < 0    Elliptical Concave  1
+        > 0  = 0    Not possible        2
+        < 0  > 0    Hyperbolic Convex   3
+        < 0  < 0    Hyperbolic Concave  4
+        < 0  = 0    Hyperbolic          5
+        = 0  > 0    Cylidrical Convex   6
+        = 0  < 0    Cylidrical Concave  7
+        = 0  = 0    Planar              8
+
+    The name of the generated arrays are: "Mean_Curvature", 
+    "Gauss_Curvature", and "Local_Shape_Type".
+    """
+    # Compute mean curvature
+    meanCurvature = vtk.vtkCurvatures()
+    meanCurvature.SetInputData(surface)
+    meanCurvature.SetCurvatureTypeToMean()
+    meanCurvature.Update()
+
+    # Compute Gaussian curvature
+    gaussianCurvature = vtk.vtkCurvatures()
+    gaussianCurvature.SetInputData(meanCurvature.GetOutput())
+    gaussianCurvature.SetCurvatureTypeToGaussian()
+    gaussianCurvature.Update()
+
+    cellCurvatures = vtk.vtkPointDataToCellData()
+    cellCurvatures.SetInputData(gaussianCurvature.GetOutput())
+    cellCurvatures.PassPointDataOff()
+    cellCurvatures.Update()
+
+    curvatureSurface = cellCurvatures.GetOutput()
+
+    # Set types of surface patches based on curvatures
+    gaussCurvatureArray = curvatureSurface.GetCellData().GetArray('Gauss_Curvature')
+    meanCurvatureArray = curvatureSurface.GetCellData().GetArray('Mean_Curvature')
+
+    # Add an int array tp hold the surface local type
+    localShapeScalars = vtk.vtkIntArray()
+    localShapeScalars.SetNumberOfComponents(1)
+    localShapeScalars.SetNumberOfTuples(curvatureSurface.GetNumberOfCells())
+    localShapeScalars.SetName('Local_Shape_Type')
+    localShapeScalars.FillComponent(0, 0)
+
+    curvatureSurface.GetCellData().AddArray(localShapeScalars)
+    # curvatureSurface.GetPointData().SetActiveScalars('Local_Shape_Type')
+
+    # Update local type based on curvatures
+    for cell in range(cellCurvatures.GetOutput().GetNumberOfCells()):
+        meanCurvature = meanCurvatureArray.GetValue(cell)
+        GaussCurvature = gaussCurvatureArray.GetValue(cell)
+
+        # Elliptical convex
+        if GaussCurvature > 0.0 and meanCurvature > 0.0:
+            localShapeScalars.SetValue(cell, 0)
+
+        # Elliptical concave
+        elif GaussCurvature > 0.0 and meanCurvature < 0.0:
+            localShapeScalars.SetValue(cell, 1)
+
+        # Apparently, not possible
+        elif GaussCurvature > 0.0 and meanCurvature == 0.0:
+            localShapeScalars.SetValue(cell, 2)
+
+        # Hyperbolic "more convex"
+        elif GaussCurvature < 0.0 and meanCurvature > 0.0:
+            localShapeScalars.SetValue(cell, 3)
+
+        # Hyperbolic "more concave"
+        elif GaussCurvature < 0.0 and meanCurvature < 0.0:
+            localShapeScalars.SetValue(cell, 4)
+
+        # Hyperbolic
+        elif GaussCurvature < 0.0 and meanCurvature == 0.0:
+            localShapeScalars.SetValue(cell, 5)
+
+        # Cylindric convex
+        elif GaussCurvature == 0.0 and meanCurvature > 0.0:
+            localShapeScalars.SetValue(cell, 6)
+
+        # Cylindric concave
+        elif GaussCurvature == 0.0 and meanCurvature < 0.0:
+            localShapeScalars.SetValue(cell, 7)
+
+        # Planar
+        elif GaussCurvature == 0.0 and meanCurvature == 0.0:
+            localShapeScalars.SetValue(cell, 8)
+
+    return curvatureSurface
+
+
+if __name__ == '__main__':
+
+    import sys
+    import polydatatools as tools
+
+    filename = sys.argv[1]
+
+    surface = tools.readSurface(filename)
+
+    curvatures = surfaceCurvature(surface)
+
+    tools.writeSurface(curvatures, '/home/iagolessa/tmp.vtp')
