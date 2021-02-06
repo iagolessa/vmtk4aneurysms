c0a6097a34680b87ada229ed41d93c8a4356f412
Iago Lessa de Oliveira <iago.lessa.oliveira@gmail.com>
2021-02-06 18:11:48 -0300

    [enh]: wallmotion.py -> included WallTypeCharacterization function to classify wall based on hemodynamics.

--- wallmotion.py
+++ wallmotion.py
@@ -2,6 +2,7 @@
 
 import sys
 import vtk
+from vtk.numpy_interface import dataset_adapter as dsa
 
 from .lib import names
 from .lib import constants as const
@@ -98,3 +99,93 @@ def AneurysmPulsatility2(lumen_surface: names.polyDataType,
 
     # Compute pulsatility
     return psAneurysm.GetAneurysmVolume()/ldAneurysm.GetAneurysmVolume() - 1.0
+
+def WallTypeCharacterization(surface: names.polyDataType,
+                             low_wss: float = 5.0,
+                             high_wss: float = 10.0,
+                             low_osi: float = 0.001,
+                             high_osi: float = 0.01) -> names.polyDataType:
+    """Based on hemodynamics, characterize wall morphology.
+
+    Based on the TAWSS and OSI fields, identifies the aneurysm regions prone to
+    atherosclerotic walls (thicker walls) and red wall (thinner) by adding a
+    new array on the passed surface name "WallType" with the following values:
+
+        0 -> normal wall;
+        1 -> atherosclerotic wall;
+        2 -> thinner wall.
+
+    Classifications based on the publications:
+
+        Furukawa et al. "Hemodynamic characteristics of hyperplastic remodeling
+        lesions in cerebral aneurysms". PLoS ONE. 2018 Jan 16;13:1–11.
+
+    and
+
+        Cebral et al. "Local hemodynamic conditions associated with focal
+        changes in the intracranial aneurysm wall". American Journal of
+        Neuroradiology.  2019; 40(3):510–6.
+    """
+    # Code
+    normalWall  = 0
+    thickerWall = 1
+    thinnerWall = 2
+
+    wallTypeArrayName = "WallType"
+    aneurysmNeckArrayName = aneu.AneurysmNeckArrayName
+
+    # Maybe put this limiting values to be passed by the user
+    # for flexibility
+    limitHemodynamics = {names.TAWSS: {"low": low_wss, "high": high_wss},
+                         names.OSI  : {"low": low_osi, "high": high_osi}#,
+                         #names.RRT  : {"low": 0.25,  "high": 0.75}
+                        }
+
+    arraysInSurface = tools.GetPointArrays(surface) + \
+                      tools.GetCellArrays(surface)
+
+    if aneurysmNeckArrayName not in arraysInSurface:
+        surface = aneu.SelectAneurysm(surface)
+
+    elif names.TAWSS not in arraysInSurface:
+        sys.exit("TAWSS array not in surface!")
+
+    elif names.OSI not in arraysInSurface:
+        sys.exit("OSI array not in surface!")
+
+    else:
+        pass
+
+    fieldsDf = tools.vtkPolyDataToDataFrame(surface)
+
+    # Add int field which will indicate the thicker regions
+    # zero indicates normal wall... the aneuysm portion wil be updated
+    fieldsDf[wallTypeArrayName] = normalWall
+
+    # Groups of conditions
+    isAneurysm = fieldsDf[aneurysmNeckArrayName] == 0.0
+
+    isHighWss = fieldsDf[names.TAWSS] > limitHemodynamics[names.TAWSS]["high"]
+    isHighOsi = fieldsDf[names.OSI] > limitHemodynamics[names.OSI]["high"]
+    # isHighRrt = fieldsDf[names.RRT] > limitHemodynamics[names.RRT]["high"]
+
+    isLowWss = fieldsDf[names.TAWSS] < limitHemodynamics[names.TAWSS]["low"]
+    isLowOsi = fieldsDf[names.OSI] < limitHemodynamics[names.OSI]["low"]
+    # isLowRrt = fieldsDf[names.RRT] < limitHemodynamics[names.RRT]["low"]
+
+    thickerWallCondition = (isAneurysm) & (isLowWss)  & (isHighOsi)# & (isHighRrt)
+    thinnerWallCondition = (isAneurysm) & (isHighWss) & (isLowOsi) # & (isLowRrt)
+
+    # Update wall type array
+    fieldsDf.loc[thickerWallCondition, wallTypeArrayName] = thickerWall
+    fieldsDf.loc[thinnerWallCondition, wallTypeArrayName] = thinnerWall
+
+    hemodynamicSurfaceNumpy = dsa.WrapDataObject(surface)
+
+    # Add new field to surface
+    hemodynamicSurfaceNumpy.CellData.append(
+        dsa.VTKArray(fieldsDf[wallTypeArrayName]),
+        wallTypeArrayName
+    )
+
+    return hemodynamicSurfaceNumpy.VTKObject
