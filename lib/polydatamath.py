b7a0b63ad95fe841bda9c474c499ffe5fa5a7dc0
Iago Lessa de Oliveira <iago.lessa.oliveira@gmail.com>
2021-10-11 16:42:35 -0300

    [add] function to compute the statistics of a field defined on a surface.

--- lib/polydatamath.py
+++ lib/polydatamath.py
@@ -5,6 +5,7 @@ from scipy.integrate import simps
 import vtk
 
 from vtk.numpy_interface import dataset_adapter as dsa
+from . import names
 from . import polydatatools as tools
 from .polydatageometry import Surface
 
@@ -73,6 +74,69 @@ def SurfaceAverage(surface, array_name):
 
     # Compute L2-norm
     return integral/surfaceArea
+
+def SurfaceFieldStatistics(
+        surface: names.polyDataType,
+        field_name: str,
+        n_percentile: float=99
+    )   -> dict:
+    """Computes a field statistics defined on a surface.
+
+    Given a surface (or a surface patch), as a vtkPolyData type, with a
+    field_name defined on it, compute the field's descriptive statistics
+    (average, maximum, minimum, and the 95 percentil by default) and the
+    surface average. Returns a dict with each statistic.
+
+    The function accepts either point or a cell array, but if a point array is
+    passed, the function first converts it to a cell array before the
+    computation of the stats.
+    """
+
+    if type(surface) != names.polyDataType:
+        raise TypeError("Need vtkPolyData surface, not {}.". format(
+                    type(surface)
+                 )
+              )
+
+    pointArrays = tools.GetPointArrays(surface)
+    cellArrays  = tools.GetCellArrays(surface)
+
+    fieldInSurface = field_name in pointArrays or field_name in cellArrays
+
+    # Check if arrays are on surface
+    if not fieldInSurface:
+        raise ValueError(
+                  "{} field not found in the surface.".format(field_name)
+              )
+
+    if field_name in pointArrays:
+        pointToCell = vtk.vtkPointDataToCellData()
+        pointToCell.SetInputData(surface)
+        pointToCell.PassPointDataOff()
+        pointToCell.Update()
+
+        surface = pointToCell.GetOutput()
+
+    # Get Array
+    npSurface = dsa.WrapDataObject(surface)
+
+    fieldOnSurface = npSurface.GetCellData().GetArray(field_name)
+
+    # Check type of field: vector or scalar
+    nComponents = fieldOnSurface.shape[-1]
+
+    if nComponents == 3 or nComponents == 6:
+        fieldOnSurface = NormL2(fieldOnSurface, 1)
+
+    # Compute statistics
+    return {"surf_avg": SurfaceAverage(surface, field_name),
+            "average": np.average(fieldOnSurface),
+            "maximum": np.max(fieldOnSurface),
+            "minimum": np.min(fieldOnSurface),
+            "percentil"+str(n_percentile): np.percentile(
+                                               fieldOnSurface,
+                                               n_percentile
+                                           )}
 
 def HadamardDot(np_array1, np_array2):
     """Computes dot product in a Hadamard product way.
