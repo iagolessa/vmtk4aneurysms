0074c575d304d8e14b2e912c0efd71607c629178
Iago Lessa de Oliveira <iago.lessa.oliveira@gmail.com>
2022-06-09 11:35:44 -0300

    Added copyright notice to each file.

--- pypescripts/vmtksurfaceprojectcellfield.py
+++ pypescripts/vmtksurfaceprojectcellfield.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2022, Iago L. de Oliveira
+
+# vmtk4aneurysms is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from __future__ import absolute_import #NEEDS TO STAY AS TOP LEVEL MODULE FOR Py2-3 COMPATIBILITY
 
 import sys
